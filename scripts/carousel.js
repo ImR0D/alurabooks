b6fd623df4d5af2b6a5eaaced6f39db74f90a0c0
ImR0D <gvr.decor@gmail.com>
2025-03-06 16:53:26 -0300

    Create card of interest and fix some styles

---
+++ scripts/carousel.js
@@ -0,0 +1,64 @@
+var swiper = new Swiper('.swiper', {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    effect: 'slide', // 'slide', 'fade', 'cube', 'coverflow', 'flip', 'creative' or 'cards'
+    //focusableElements: 'swiper-explainable',
+    direction: 'horizontal',
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        type: 'bullets',
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+        delay: 8000,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 2,
+        },
+        428: {
+            slidesPerView: 3,
+        },
+        1728: {
+            slidesPerView: 4,
+        },
+    },
+    on: {
+        click: true,
+    }        
+});
+
+swiper.on('click', function(){
+    const clickedElement = swiper.clickedSlide;
+    var targetElement = clickedElement;
+    if (clickedElement.classList.contains('swiper-slide-active')) {
+        targetElement = swiper.clickedSlide.children[1];
+        if (targetElement.classList.contains('hidden-element')) {
+            targetElement.classList.remove("hidden-element");
+        } else {
+            targetElement.classList.add("hidden-element");
+        }
+    }
+});
+swiper.on('slideChangeTransitionStart', function () {
+    const clickedElement = swiper.clickedSlide;
+    var targetElement = clickedElement;
+    swiper.slides.forEach(element => {
+        if (element.classList.contains('swiper-slide-active')) {
+            element.children[1].classList.remove('hidden-element');
+        }
+    });
+});
+swiper.on('slideChangeTransitionEnd', function () {
+    const clickedElement = swiper.clickedSlide;
+    var targetElement = clickedElement;
+    swiper.slides.forEach(element => {
+        if (element.classList.contains('swiper-slide-prev') || element.classList.contains('swiper-slide-next')) {
+            element.children[1].classList.add('hidden-element');
+        }
+    });
+});
